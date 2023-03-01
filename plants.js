25e28b994cac8a68a92e15186e308e95c5914c12
Jamie Caudill <j.caudill7177@gmail.com>
2023-03-01 13:55:26 -0800

    Add another if statment

--- plants.js
+++ plants.js
@@ -1,8 +1,11 @@
-var get_sunshine = true;
+var get_sunshine = false;
 var ounces_of_water = 11;
 
 if(get_sunshine === true && ounces_of_water >= 10) {
-    console.log("Your plant is thriving!");
+    console.log("You have a happy plant");
+} else if(get_sunshine === true || ounces_of_water >=10) {
+    console.log("Your plant is alive");
 } else {
     console.log("Your plant is dead.");
 }
+
