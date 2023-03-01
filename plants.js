808f6a13ebc4a4c42d8df8282e8c3c0d6b504093
Jamie Caudill <j.caudill7177@gmail.com>
2023-03-01 13:47:55 -0800

    Modify if statement

--- plants.js
+++ plants.js
@@ -1,6 +1,8 @@
-var get_sunshine = true
-var ounces_of_water = 4
+var get_sunshine = true;
+var ounces_of_water = 11;
 
 if(get_sunshine === true && ounces_of_water >= 10) {
-    console.log("Your plant is thriving!")
+    console.log("Your plant is thriving!");
+} else {
+    console.log("Your plant is dead.");
 }
